d4658da554131a9cbf083589ad45bacc3bc44c9f
Reyvand Jasper Medrano <pawsmedz@gmail.com>
2024-09-11 20:34:44 +0800

    cloudinary files storage implementation

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -26,43 +26,38 @@ class TodoController extends Controller
             'type' => 'required|string',
             'title' => 'required|string|max:255',
             'description' => 'nullable|string',
-            'file' => 'nullable|file|mimes:jpg,jpeg,png,pdf|max:2048',
+            'fileUrl' => 'nullable|url', 
         ]);
-
-        $todoData = $request->only(['type', 'title', 'description']);
-
-        if ($request->hasFile('file')) {
-            $file = $request->file('file')->store('files', 'public');
-            $todoData['file'] = $file;
+    
+        $todoData = $request->only(['type', 'title', 'description', 'fileUrl']);
+    
+        if (isset($todoData['fileUrl'])) {
+            $todoData['file'] = $todoData['fileUrl']; 
+            unset($todoData['fileUrl']);
         }
-
+    
         $todo = Todo::create($todoData);
+    
         return response()->json($todo, 201);
     }
+    
     public function update(Request $request, $id)
     {
         \Log::info('Incoming Request Data:', $request->all());
+
         $request->validate([
             'type' => 'required|string',
             'title' => 'required|string|max:255',
             'description' => 'required|string',
-            'file' => 'nullable|string', 
+            'fileUrl' => 'nullable|url', 
         ]);
     
         $todo = Todo::findOrFail($id);
-        \Log::info('Full Request Data:', $request->all());
         $todoData = $request->only(['type', 'title', 'description']);
-        if ($request->has('file')) {
-            if ($todo->file) {
-                Storage::disk('public')->delete($todo->file);
-            }
-            $file = $request->input('file'); 
-            $todoData['file'] = $file;
-        } else {
-            $todoData['file'] = $todo->file;
+
+        if ($request->filled('fileUrl')) {
+            $todoData['file'] = $request->input('fileUrl');
         }
-    
-        \Log::info('Todo Data to be Updated:', $todoData);
     
         $todo->update($todoData);
     
@@ -70,20 +65,11 @@ class TodoController extends Controller
         return response()->json($todo);
     }
     
-
     
-    
-
-
-
-
-
-
     public function destroy($id)
     {
         $todo = Todo::findOrFail($id);
 
-        // Delete the file if it exists
         if ($todo->file) {
             Storage::disk('public')->delete($todo->file);
         }
